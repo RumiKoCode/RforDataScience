e03fe6dd066a8b25f4ae5f97fe3193ff5083fba7
RumiKoCode <1875192575@qq.com>
2024-04-02 12:44:58 +0800

    测试dlookr包

--- RBasic02.R
+++ RBasic02.R
@@ -1,2 +1,12 @@
 #这是一个新的Rstudio与GitHub连接的测试
 #测试目的是本地R语言文件能否推送到GitHub仓库中
+library(tidyverse)
+library(ggplot2)
+library(dlookr)
+
+head(mpg)
+diagnose(mpg)
+diagnose_outlier(mpg)
+ggplot(mpg,aes(x=mpg$cty))+
+  geom_boxplot()+
+  coord_flip()
